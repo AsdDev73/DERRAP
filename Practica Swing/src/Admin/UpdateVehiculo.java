025fe12c31ffe23b9152dd6ef545437ab7406fad
AsdDev73 <theefto1@gmail.com>
2025-01-13 19:31:07 +0100

    arreglo jframe mal escrito por fer

--- Practica Swing/src/Admin/UpdateVehiculo.java
+++ Practica Swing/src/Admin/UpdateVehiculo.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class UpfateVehiculo extends JFrame {
+public class UpdateVehiculo extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -18,7 +18,7 @@ public class UpfateVehiculo extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UpfateVehiculo frame = new UpfateVehiculo();
+					UpdateVehiculo frame = new UpdateVehiculo();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class UpfateVehiculo extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public UpfateVehiculo() {
+	public UpdateVehiculo() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
